d742ddb6bf34e3fd544c4d82bf661705290411bf
edwardcaoyue <caoyue24@huawei.com>
2025-10-20 14:47:21 +0800

    focus消息新增参数字段
    Signed-off-by: edwardcaoyue <caoyue24@huawei.com>

--- frameworks/core/frame_aware_policy/include/intellisense_server.h
+++ frameworks/core/frame_aware_policy/include/intellisense_server.h
@@ -38,7 +38,7 @@ public:
     void ReportAppInfo(const int pid, const int uid, const std::string bundleName, ThreadState state);
     void ReportProcessInfo(const int pid, const int uid, const std::string bundleName, ThreadState state);
     void ReportCgroupChange(const int pid, const int uid, const int oldGroup, const int newGroup);
-    void ReportWindowFocus(const int pid, const int uid, int isFocus);
+    void ReportWindowFocus(const int pid, const int uid, int isFocus, int displayId);
     void ReportRenderThread(const int pid, const int uid, int renderTid);
     void ReportContinuousTask(const int pid, const int uid, const int status);
     bool ReadXml();

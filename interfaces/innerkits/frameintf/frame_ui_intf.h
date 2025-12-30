67057b410a49a6896b7a6a0a029d5997913faa2a
ymw1997 <yemingwei1@huawei.com>
2025-12-30 16:06:34 +0800

    update: 更新文件 frame_ui_intf.h
    Signed-off-by: ymw1997 <yemingwei1@huawei.com>

--- interfaces/innerkits/frameintf/frame_ui_intf.h
+++ interfaces/innerkits/frameintf/frame_ui_intf.h
@@ -45,7 +45,7 @@ public:
     void BeginProcessPostFlush() const;
     void ProcessCommandsStart() const;
     void AnimateStart() const;
-    void RenderStart(uint64_t timestamp, int skipFirstFrame = 0) const;
+    void RenderStart(uint64_t timestamp) const;
     void RenderEnd() const;
     void SendCommandsStart() const;
 
@@ -63,28 +63,9 @@ public:
     void MonitorGpuStart(uint32_t fenceId) const;
     void MonitorGpuEnd() const;
     void SendFenceId(uint32_t fenceId) const;
-
-    void DirectRenderEnd();
-    void UniRenderStart();
-    void UniRenderEnd();
-    void CheckUnblockMainThreadPoint();
-    void CheckPostAndWaitPoint();
-    void CheckBeginFlushPoint();
-    void ReportBufferCount(int count);
-    void ReportHardwareInfo(int tid);
-    void ReportFrameDeadline(int deadline, uint32_t currentRate);
-    void ReportUnmarshalData(int unmarshalTid, size_t dataSize);
-    void ReportDDGRTaskInfo();
-    void ReportScbSceneInfo(const std::string& description, bool eventStatus);
 private:
     bool inited = false;
 };
-
-extern "C" {
-    void Init();
-    void ReportSchedEvent(OHOS::RME::FrameSchedEvent event,
-        const std::unordered_map<std::string, std::string>& payload);
-}
 } // namespace RME
 } // namespace OHOS
 #endif
